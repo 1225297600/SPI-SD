65afbf2b6b5f552d95775809cebce05f662ded4e
张治灵 <50475813+1225297600@users.noreply.github.com>
2023-10-09 11:29:53 +0800

    Update drv_spi_sd.h

--- drv_spi_sd.h
+++ drv_spi_sd.h
@@ -6,24 +6,6 @@
 #define 	SD_TRY_TIMES 		100
 
 typedef enum{
-	SD_CMD0=0,			//restart
-	SD_CMD1=1,			//mmc init
-	SD_CMD8=8,			//status
-	SD_CMD9=9,			//read data reg
-	SD_CMD12=12,		//read more stop
-	SD_CMD10=10,		//read sign reg
-	SD_CMD16=16,		//set block Byte size
-	SD_CMD17=17,		//read a block
-	SD_CMD18=18,		//read more block
-	SD_CMD23=23,		//erase a block
-	SD_CMD24=24,		//write a block
-	SD_CMD25=25,		//write more block
-	SD_CMD41=41,		//messig & init
-	SD_CMD55=55,		//next cmd
-	SD_CMD58=58,		//read OCR reg
-}SD_CMD;
-
-typedef enum{
 	SD_TYPE_INIT_FAIL=0,
 	SD_TYPE_V1_SD,
 	SD_TYPE_V1_MMC,
